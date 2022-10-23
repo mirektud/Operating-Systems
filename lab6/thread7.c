345d5dc72bd660f1b8bff29453bc604c16767e04
mirektud <54992226+mirektud@users.noreply.github.com>
2022-10-23 21:30:01 +0100

    Update thread7.c

--- lab6/thread7.c
+++ lab6/thread7.c
@@ -2,8 +2,10 @@
 #include<stdio.h>
 
 void * function(void * ptr){
-	printf("Hello world, this is child thread!\n"); int *x = (int*) ptr;
-	printf("I received an integer: %d\n", *x); pthread_exit(NULL);
+	printf("Hello world, this is child thread!\n"); 
+        int *x = (int*) ptr;
+	printf("I received an integer: %d\n", *x); 
+        pthread_exit(NULL);
 }
 
 int main(){
@@ -16,7 +18,8 @@ int main(){
 	// a pointer to the address of x will be passed 
 	int * ptr = &x;
 	
-	int r = pthread_create(&t_id, NULL, function, (void *)ptr); pthread_join(t_id, NULL);
+	int r = pthread_create(&t_id, NULL, function, (void *)ptr); 
+        pthread_join(t_id, NULL);
 	printf("Master thread is exiting\n");
 }
 
