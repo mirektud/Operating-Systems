bcc7ec3261b426cfaef58e94f07cf310d33ee5ff
mirektud <54992226+mirektud@users.noreply.github.com>
2024-10-20 18:41:59 +0100

    Update m7.c

--- lab7/m7.c
+++ lab7/m7.c
@@ -33,7 +33,7 @@ int main(int argc, char * argv[]) {
   
     pthread_create( & tid1, NULL, f1, (void*) argv[1]);
     pthread_create( & tid2, NULL, f1, (void*) argv[2]);
-    pthread_create( & tid2, NULL, f1, (void*) argv[3]);
+    pthread_create( & tid3, NULL, f1, (void*) argv[3]);
   
     pthread_join(tid1, NULL);
     pthread_join(tid2, NULL);
